078bf24789cb927f3acb2630003fc5f43aa88e99
shoji <s.shoji39@gmail.com>
2021-08-04 11:40:10 +0900

    投稿機能の実装

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Tweet;
+use Illuminate\Support\Facades\Auth;
+
 class TweetController extends Controller
 {
     public function index(Tweet $tweet)
@@ -18,5 +20,18 @@ class TweetController extends Controller
     public function showCreateForm()
     {
         return view('tweets/create');
+    }
+
+    public function create(Request $request)
+    {
+        // $user = Auth::user();
+        
+        $tweet = new Tweet();
+        $tweet->title = $request->title;
+        $tweet->content  = $request->content;
+        $tweet->user_id  = Auth::id();
+        $tweet->save();
+
+        return redirect()->route('tweets.index');
     }
 }
