0bce8a981a651ea71a8839ff7e084cbb5da9c496
shoji <s.shoji39@gmail.com>
2021-08-05 09:59:59 +0900

    comments_table作成

---
+++ database/migrations/2021_08_05_004009_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('comments');
+            $table->integer('tweet_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('tweet_id')->references('id')->on('tweets');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
